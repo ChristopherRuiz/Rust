829c445740882853ba16636be936d0c687d2600f
Christopher Ruiz <christophersruiz7@gmail.com>
2021-02-10 16:20:30 -0800

    Fix

--- hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075.d
+++
@@ -1,5 +0,0 @@
-/home/chris/Desktop/Rust/hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075: src/main.rs
-
-/home/chris/Desktop/Rust/hello_cargo/target/debug/deps/hello_cargo-adbecd80743cb075.d: src/main.rs
-
-src/main.rs:
